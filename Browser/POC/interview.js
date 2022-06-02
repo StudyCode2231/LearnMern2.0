c4075e32c951237d795931154c6d9514de7d2d3e
StudyCode2231 <1903044@kiit.ac.in>
2022-06-02 21:48:00 +0530

    :zap: file updated

--- Browser/POC/interview.js
+++ Browser/POC/interview.js
@@ -1,27 +1,168 @@
-//1st ques
-let arr = [1, 4, 2, 3, 2, 4, 1, 5, 6, 1, 1];
-//output {1: 4, 2: 2, 3: 1, 4: 2, 5: 1, 6: 1}
-let obj = {};
-for (let i = 0; i < arr.length; i++) {
-  obj[arr[i]] = obj[arr[i]] == undefined ? 1 : obj[arr[i]] + 1;
+// //1st ques
+// let arr = [1, 4, 2, 3, 2, 4, 1, 5, 6, 1, 1];
+// //output {1: 4, 2: 2, 3: 1, 4: 2, 5: 1, 6: 1}
+// let obj = {};
+// for (let i = 0; i < arr.length; i++) {
+//   obj[arr[i]] = obj[arr[i]] == undefined ? 1 : obj[arr[i]] + 1;
+// }
+// console.log(obj);
+
+// let Obj = {
+//     name: "Abhishek",
+//     age: 30,
+//     email: "talk2me@gmail.com",
+//     phone:undefined
+// };
+
+// console.log(Obj.email); // 
+// console.log(Obj.ac); //undefined
+// console.log(Obj.phone); //will think phone property does not exists
+// console.log("phone" in Obj); //true 
+
+// for (key in Obj) {
+//     console.log(key);
+//     console.log(Obj[key]);
+// }
+
+// let user={
+//     name:"Abhi",
+//     age:30
+// }
+// let clone={};
+
+// Object.assign(clone,user);
+// //{name: 'Abhi', age: 30}
+// clone
+// //{name: 'Abhi', age: 30}
+// clone.name = "Abhishek"
+// Object.assign(clone, user, { name: "Abhishek Goel" }); //replace the name propert
+// Object.assign(clone, user, { fullName: "Abhishek Goel" }); //adds fullName property
+
+
+// //deep clone 
+
+// let user = {
+//   name: "John",
+//   sizes: {
+//     height: 182,
+//     width: 50,
+//   },
+// };
+// let clone = {};
+// //this is not deep cloning , because refference to size identifier is same in both clone and user obj 
+// Object.assign(clone, user);
+// console.log(clone);
+// clone.name = "ABHISHEK";
+// console.log(clone);
+// clone.sizes.height = 123;
+// console.log(clone);
+// console.log(user);
+
+// //flatten an Object 
+
+// let flattenedObj={ 
+//   "name": "Jhon",
+//   "sizes.height" : 182,
+//   "sizes.width":50
+// }
+// //it only deep clones the properties in an obj and not the method 
+// JSON.stringify()
+
+// const c = 20;
+// c = 23; //error
+
+// const user1 = #f45d3 
+// #f45d3 ->
+// {
+//   name: "Abhi",
+//   age: 30,
+//   email:"talk2me@gmail.com"
+// }
+
+// user.name = "Abhi";
+// console.log(user);
+
+
+// let user = {
+//   name: "Abhishek",
+//   sayHi() { 
+//     console.log(this.name);
+//   }
+// }
+
+// let admin = user;
+
+// user = null;
+// user.sayHi();
+
+// admin.sayHi();
+//<----- rules---->
+  //1. The value of this is evaluated during the run-time, depending on the context.
+  //2. t
+// this interviewques types
+
+//1 
+
+function type1() {
+  console.log(this.name);
 }
-console.log(obj);
 
-let Obj = {
-    name: "Abhishek",
-    age: 30,
-    email: "talk2me@gmail.com",
-    phone:undefined
-};
+var name = "JS";
 
-console.log(Obj.email); // 
-console.log(Obj.ac); //undefined
-console.log(Obj.phone); //will think phone property does not exists
-console.log("phone" in Obj); //true 
+type1();  //globally , this -> window , 
 
-for (key in Obj) {
-    console.log(key);
-    console.log(Obj[key]);
+//2
+
+function type2() {
+  console.log(this.lname);
 }
 
-//2nd ques
+var lname = "Kumar";
+
+var obj = {
+  lname: "Verma",
+  type2
+}
+
+obj.type2();//in execution context of function, 'this' gives reference to the obj
+
+
+//3
+
+var food = "Pizaa";
+
+var obj = {
+  food: "Pasta",
+  eat() {
+    console.log("i am eating " + this.food);
+  }
+}
+
+var foo = obj.eat;
+
+foo();
+
+
+
+
+
+// 'use strict'
+
+// var z;
+// console.log(z); 
+
+// y = 10;
+// console.log(y); //10 
+
+// console.log(x); //error , because memory is allocated on variable declaration , but in this cases variable has not been declsred , intilaized 
+// x = 6;  
+
+
+// function sayHi() {
+//   //undefined in case of node, "" in case of browser (non strict )
+//   // strict mode-> error in both the cases
+//   console.log(this.name); 
+// }
+// sayHi();
+
+
--- Browser/POC/this.js
+++ Browser/POC/this.js
@@ -1,10 +1,21 @@
-function type2(){
-console.log(this.iname);
-}
-var iname="kumar";
+// function type2(){
+// console.log(this.iname);
+// }
+// var iname="kumar";
+// var obj={
+//     iname:"verma",
+//     type2
+// }
+// obj.type2();
+// type2()
+
+//3
+var food="Piza"
 var obj={
-    iname:"verma",
-    type2
+    food:'Pasta',
+    eat(){
+        console.log("i am eating "+this.food )
+    }
 }
-obj.type2();
-type2()
+var fo=obj.eat;
+fo();
