8b25f4118ebb1d0dd60ac352f52958233180de7b
StudyCode2231 <1903044@kiit.ac.in>
2022-07-04 07:44:33 +0530

    :zap: file updated

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Components/Todo';
 
 function App() {
   return (
-  <>
-    
-  </>
+  //  <h1>Hello from the other side </h1>
+    <Todo/>
   );
 }
 
