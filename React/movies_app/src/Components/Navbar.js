2b7d1d7398c8fc4981ebc96c245ae99b6c591f23
StudyCode2231 <1903044@kiit.ac.in>
2022-07-05 23:47:54 +0530

    :construction: file updated

---
+++ React/movies_app/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div style={{display:'flex',background:'lightblue',padding:'2rem',justifyContent:'center',font-family:'';}}>
+      <h1>Movies App</h1>
+      <h1>Favourites</h1>
+
+      </div>
+    )
+  }
+}
