fa604f1ddc78a20c3b31f6c5b13c3e3c036385b1
StudyCode2231 <1903044@kiit.ac.in>
2022-07-06 21:45:29 +0530

    :tada:Todo app completed & Movies App started working on it !!!!!!!!!!

--- React/movies_app/src/App.js
+++ React/movies_app/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Banner from './Components/Banner';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
-  <Navbar/>
+    <>
+        <Navbar/>
+          
+        <Banner/>
+    </>
   );
 }
 
--- React/movies_app/src/Components/Navbar.js
+++ React/movies_app/src/Components/Navbar.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display:'flex',background:'lightblue',padding:'2rem',justifyContent:'center',font-style: 'Oswald', sans-serif;}}>
-      <h1>Movies App</h1>
-      <h1>Favourites</h1>
+      <div style={{display:'flex',background:'lightblue',padding:'2rem',justifyContent:'space-evenly',}}>
+      <h1>MoviesApp </h1>
+      <h1> Favourites</h1>
 
       </div>
     )
