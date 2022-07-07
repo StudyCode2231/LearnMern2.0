9dd741bbfa79bc3e5ed83e7f02af4221da6f2f03
StudyCode2231 <1903044@kiit.ac.in>
2022-07-07 21:44:01 +0530

    :tada: file update

--- React/movies_app/src/Components/Navbar.js
+++ React/movies_app/src/Components/Navbar.js
@@ -3,10 +3,9 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div style={{display:'flex',background:'lightblue',padding:'2rem',justifyContent:'space-evenly',}}>
-      <h1>MoviesApp </h1>
-      <h1> Favourites</h1>
-
+        <div style={{display:'flex', background:"lightblue", padding:'1rem', justifyContent:"center", alignItems:"center"}}>
+            <h1>Movies App</h1>
+            <h2 style={{marginLeft:"2rem",}}>Favourites</h2>
       </div>
     )
   }
