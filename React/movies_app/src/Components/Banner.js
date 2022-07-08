8a7c9baccb875186252f5182cb6edb7aed30df9f
StudyCode2231 <1903044@kiit.ac.in>
2022-07-08 19:38:09 +0530

    :zap: file updated

--- React/movies_app/src/Components/Banner.js
+++ React/movies_app/src/Components/Banner.js
@@ -12,15 +12,19 @@ export default class Banner extends Component {
                 <span className="visually-hidden">Loading...</span>
               </div>
             ) : (
-              <div className="card banner-card">
-                            <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}className="card-img-top" alt="..." />
-                <div className="card-body">
-                                <h5 className="card-title">{movie.original_title}</h5>
-                  <p className="card-text">
-                    {movie.overview}
-                  </p>
-                  {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
-                </div>
+              <div className="card banner-card "  >
+                <img style={{height:'80vh'}}
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  className="card-img-top banner-img"
+                  alt="..."
+                />
+                {/* <div className="card-body "> */}
+                <h5 className="card-title banner-title">
+                  {movie.original_title}
+                </h5>
+                <p className="card-text banner-text">{movie.overview}</p>
+                {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                {/* </div> */}
               </div>
             )}
           </>
--- React/movies_app/src/Components/Navbar.js
+++ React/movies_app/src/Components/Navbar.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-        <div style={{display:'flex', background:"	#404040", padding:'1rem', justifyContent:"center", alignItems:"center", }}>
-            <h1 style={{color:"#D9E1E5"}}>Movies App</h1>
+        <div style={{display:'flex', background: '#1D2228', padding:'0.5rem', justifyContent:"center", alignItems:"center", }}>
+            <h1 style={{color:"#D9E1E5"}}>MoviesApp</h1>
             <h1 style={{marginLeft:"2rem", color:"#D9E1E5"}}>Favourites</h1>
       </div>
     )
