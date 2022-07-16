376db65b361befc10cb2a8146c56042f27550a64
StudyCode2231 <1903044@kiit.ac.in>
2022-07-16 22:40:41 +0530

    :zap: file updated

--- React/movies_app/src/App.js
+++ React/movies_app/src/App.js
@@ -1,16 +1,24 @@
-import react , { Component}  from 'react';
-import         { render   }  from 'react-dom';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Banner from './Components/Banner';
-import List from './Components/List';
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Banner />
-      <List/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
