66edec19c3aaa227dd4cdd4c763000da812ac096
StudyCode2231 <1903044@kiit.ac.in>
2022-08-17 22:14:22 +0530

    :rocket: file updated

--- React/reels/pages/signup/index.js
+++ React/reels/pages/signup/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import Image from 'next/image';
 import Logo from '../../asset/Instagram.png'
 function index() {
@@ -10,6 +11,11 @@ function index() {
         <TextField id="outlined-basic" size='small' label="Email" variant="outlined" fullWidth margin='dense' />
         <TextField id="outlined-basic" size='small' label="Password" variant="outlined" fullWidth margin='dense' />
         <TextField id="outlined-basic" size='small' label="Full Name" variant="outlined" fullWidth margin='dense' />
+
+        <Button color="secondary" variant="outlined" component="label"  fullWidth>
+        Upload
+        <input hidden accept="image/*" multiple type="file" />
+      </Button>
         </div>
     </div>
   )
