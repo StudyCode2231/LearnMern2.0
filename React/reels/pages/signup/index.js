70c4aef7d66d4efbfa56e36b71b25cbc947088ba
StudyCode2231 <1903044@kiit.ac.in>
2022-08-22 23:49:16 +0530

    :rocket: file updated

--- React/reels/pages/signup/index.js
+++ React/reels/pages/signup/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Image from 'next/image';
+import IconButton from '@mui/material/IconButton';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import Logo from '../../asset/Instagram.png'
 function index() {
   return (
@@ -11,13 +13,32 @@ function index() {
         <TextField id="outlined-basic" size='small' label="Email" variant="outlined" fullWidth margin='dense' />
         <TextField id="outlined-basic" size='small' label="Password" variant="outlined" fullWidth margin='dense' />
         <TextField id="outlined-basic" size='small' label="Full Name" variant="outlined" fullWidth margin='dense' />
+        
 
-        <Button color="secondary" variant="outlined" component="label"  fullWidth>
-        Upload
+        <Button color="secondary" variant="outlined" component="label"  fullWidth size="small">
+         <IconButton color="secondary">
+         <CloudUploadIcon/>
+        </IconButton>
+        Upload  Profile Image 
+
         <input hidden accept="image/*" multiple type="file" />
       </Button>
+
+      <Button style={{marginTop:"1rem"}} variant="contained" component="label"  fullWidth>
+        Sign Up 
+      </Button>
+      <div className='tnc'>By signing up , you agree to our Term ,
+          Condition & Cookies Policy
+        </div>
+        </div>
+        
+        <div className='bottom-card'>
+          Already Having an account ? 
+            <span style={{color:"blueviolet"}}>Login</span> 
         </div>
     </div>
+
+
   )
 }
 
